3c8434060af6aa2128dab4b458cab0aadce40c78
iwannafly <xanderavd@gmail.com>
2013-08-15 05:26:58 +0700

    bitching extJS's charts implementation

---
+++ Gauge.js
@@ -0,0 +1,49 @@
+Ext.onReady(function(){ 
+    var store = Ext.create('Ext.data.JsonStore', {
+		fields: ['name', 'value'],
+		data: [
+			{'name': 'answer to life the universe and everything', 'value':42},
+		]
+	});
+
+    Ext.create('Ext.panel.Panel', {
+       renderTo: Ext.getBody(),
+       title: 'Bitching gauge',
+       width: 400,
+       height: 300,
+       layout: 'fit',
+       items:[{
+			xtype: 'chart',
+            style: 'background:#fff',
+            animate: {
+                easing: 'bounceOut',
+                duration: 500
+            },
+            store: store,
+            insetPadding: 25,
+            flex: 1,
+                    legend: true,
+            axes: [{
+                type: 'gauge',
+                position: 'gauge',
+                minimum: 0,
+                maximum: 100,
+                steps: 10,
+                margin: 7
+            }],
+            series: [{
+                type: 'gauge',
+                field: 'value',
+                donut: 20,
+                colorSet: ['#3AA8CB', '#ddd'],
+				showInLegend: true,
+				label: {
+					field: 'name',
+					display: 'under',
+					contrast: true,
+					font: '18px Arial'
+				}
+            }]
+		}]
+     });
+});
